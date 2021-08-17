bb783d1a5c04d2ff35392690acef5d8c2206a9f2
Simon Denman <s.denman@qut.edu.au>
2021-08-17 18:35:41 +1000

    Update pascalvoc.py

--- pascalvoc.py
+++ pascalvoc.py
@@ -55,7 +55,7 @@ import _init_paths
 from BoundingBox import BoundingBox
 from BoundingBoxes import BoundingBoxes
 from Evaluator import *
-from utils import BBFormat
+from utils_metrics import BBFormat
 
 
 # Validate formats
